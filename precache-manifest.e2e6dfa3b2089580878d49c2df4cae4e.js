a86c32a023a70d66fdc1b47d2fcefbb4f01b110f
Aditya Kumar SIngh <adityasingh9501@gmail.com>
2022-07-29 13:31:19 +0530

    Updates

---
+++ precache-manifest.e2e6dfa3b2089580878d49c2df4cae4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a69b155bb73fd9dc0e4",
+    "url": "/my-app/static/css/main.6753006d.chunk.css"
+  },
+  {
+    "revision": "1a69b155bb73fd9dc0e4",
+    "url": "/my-app/static/js/main.1a69b155.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/my-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "fc07b3293d50ce2d434b29dff9069bb3",
+    "url": "/my-app/index.html"
+  }
+];
