b51e8078726abca401e3f3a2d596d1d091ab0583
Aditya Kumar SIngh <adityasingh9501@gmail.com>
2022-07-29 13:23:05 +0530

    My Text Utils App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+import Alert from './components/Alert';
+
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { Routes, Route  } from "react-router-dom";
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      showAlert('Dark mode Ativated', 'success');
+      document.body.style.backgroundColor = 'black';
+    }
+    else{
+      setMode('light');
+      showAlert('Light mode Ativated', 'success');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+  const showAlert = (mesage, type) =>{
+    setAlert({
+      msg:mesage,
+      type:type
+    });
+
+    setTimeout(() => setAlert(null), 1500)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <TextForm title="Enter text analyze" mode={mode} showAlert={showAlert}/>
+    
+    {/* <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+    <Routes>
+      <Route exact path="/" element={<TextForm title="Enter text analyze" mode={mode} showAlert={showAlert}/>} />
+      <Route exact path="/about" element={<About />} />
+        
+      
+    </Routes>
+    </Router> */}
+      
+      
+      
+      
+      
+      
+      
+    </>
+     
+      
+    
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+
+
+
+export default function TextForm
+(props) {
+    const [text, setText] = useState('');
+    
+
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        props.showAlert("Submitted", "success");
+        setText(newText);
+    }
+
+    const handleOnChange = (event) => {
+        
+        setText(event.target.value);
+        
+        
+    }
+
+  return (
+    <>
+        <div className={`mb-3 container mt-5  text-${props.mode==='light'? 'secondary': 'light'}`}>
+            <label htmlFor="myBox" className="form-label" ><b>{props.title}</b></label>
+            <textarea className="form-control" 
+                id="exampleFormControlTextarea1" 
+                rows="3" value={text} 
+                onChange={handleOnChange}
+                placeholder="Enter something"
+                style={{backgroundColor: props.mode==="light"?"white":"darkgray"}}
+                >
+            </textarea>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button type="button" className="btn btn-dark my-4" onClickCapture={handleUpClick}>Submit</button>
+            </div>
+        </div>
+        
+        <div className={`container text-${props.mode==='light'? 'secondary': 'light'}`}>
+            <h4>Your text summary</h4>
+            <p>Number of characters <b>{text.length}</b>, number of words <b>{text.length===0?0:text.replaceAll(/\s{2,}/g, ' ').trim().split(" ").length}</b></p>
+        </div>
+    </>
+  )
+}
